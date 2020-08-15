4d43907390c94b0d8fd7bcbba5da2897b73e47bf
Maxime Moreillon <m.moreillon@gmail.com>
2020-08-15 04:11:41 +0000

    serving standalone

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+import VueSocketIOExt from 'vue-socket.io-extended'
+import io from 'socket.io-client'
+
+const socket = io(process.env.VUE_APP_HOME_AUTOMATION_API_URL)
+
+Vue.use(VueSocketIOExt, socket);
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="current_location_wrapper">
+    <img class="current_location" :src="location_image_src" alt="">
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
+  },
+  data(){
+    return {
+      current_location: 'unknown'
+    }
+  },
+  sockets: {
+    connect() {
+      console.log('socket connected')
+    },
+    location(message){
+      this.current_location = message
+    }
+  },
+  computed: {
+    location_image_src(){
+
+      switch (this.current_location) {
+        case 'living':
+          return require('@/assets/locations/living.svg')
+        case 'kitchen':
+          return require('@/assets/locations/kitchen.svg')
+        case 'bathroom':
+          return require('@/assets/locations/bathroom.svg')
+        case 'toilet':
+          return require('@/assets/locations/toilet.svg')
+        case 'bedroom':
+          return require('@/assets/locations/bedroom.svg')
+        case 'out':
+          return require('@/assets/locations/location-exit.svg')
+        default:
+          //return require('@/assets/locations/location-exit.svg')
+          return require('@/assets/locations/crosshairs-question.svg')
+      }
+    }
   }
+
 }
 </script>
+
+<style scoped>
+.current_location_wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.current_location {
+  width: 70vw;
+  height: 60vh;
+  object-fit: contain;
+}
+</style>
