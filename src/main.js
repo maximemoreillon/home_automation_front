a702c220989e424b378b9c8aa85bf1949df04f95
Maxime Moreillon <m.moreillon@gmail.com>
2023-02-05 07:44:16 +0900

    baseUrl

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import axios from "axios"
+import VueAxios from "vue-axios"
 
-import VueSocketIOExt from 'vue-socket.io-extended'
-import io from 'socket.io-client'
-import vuetify from './plugins/vuetify'
-import './registerServiceWorker'
+import VueSocketIOExt from "vue-socket.io-extended"
+import io from "socket.io-client"
+import vuetify from "./plugins/vuetify"
+import "./registerServiceWorker"
 
-const socket = io(process.env.VUE_APP_HOME_AUTOMATION_API_URL)
+const { VUE_APP_HOME_AUTOMATION_API_URL } = process.env
 
-Vue.use(VueSocketIOExt, socket);
+const socket = io(VUE_APP_HOME_AUTOMATION_API_URL)
+axios.defaults.baseURL = VUE_APP_HOME_AUTOMATION_API_URL
+
+Vue.use(VueSocketIOExt, socket)
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
@@ -21,5 +24,5 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app")
