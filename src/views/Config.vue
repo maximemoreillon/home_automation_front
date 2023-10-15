c5834bc7f7f028249844deb4c25c3035ce6f65a3
Maxime Moreillon <m.moreillon@gmail.com>
2023-10-15 10:53:55 +0900

    config

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -1,11 +1,15 @@
 <template>
   <v-card :loading="loading">
     <v-toolbar flat>
-      <v-card-title> Config </v-card-title>
+      <v-card-title> Devices </v-card-title>
     </v-toolbar>
-    <v-card-text v-for="room in rooms" :key="room.name">
-      <h2>{{ room.name }}</h2>
-      <v-data-table :headers="headers" :items="room.devices"> </v-data-table>
+    <v-card-text>
+      <v-data-table
+        :headers="headers"
+        :items="devices"
+        group-by="room"
+        show-group-by
+      />
     </v-card-text>
   </v-card>
 </template>
@@ -15,9 +19,10 @@ export default {
   name: "Config",
   data() {
     return {
-      rooms: [],
+      devices: [],
       loading: false,
       headers: [
+        { text: "Room", value: "room" },
         { text: "Type", value: "type" },
         { text: "Status topic", value: "statusTopic" },
         { text: "Command topic", value: "commandTopic" },
@@ -31,7 +36,13 @@ export default {
     async getRooms() {
       this.loading = true
       const { data } = await this.axios.get("/rooms")
-      this.rooms = data
+      this.devices = data.reduce(
+        (prev, room) => [
+          ...prev,
+          ...room.devices.map((d) => ({ room: room.name, ...d })),
+        ],
+        []
+      )
       this.loading = false
     },
   },
